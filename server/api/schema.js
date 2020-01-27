7b971a71f4ecc07de2031cc40aa21c0cf0e21e19
fostlt <lanceadamp@gmail.com>
2020-01-26 17:35:01 -0800

    boomtown remake

---
+++ server/api/schema.js
@@ -0,0 +1,67 @@
+const { gql } = require("apollo-server-express");
+
+module.exports = gql`
+  # scalar Date
+
+  type Item {
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String!
+    itemowner: User!
+    tags: [Tag]
+    # Add date instead of string
+    created: String!
+    borrower: User
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items: [Item]
+    borrowed: [Item]
+  }
+
+  type Tag {
+    id: ID!
+    title: String!
+  }
+
+  type AuthPayload {
+    token: String
+    user: User
+  }
+
+  input AssignedTag {
+    id: ID!
+    title: String!
+  }
+
+  input AssignedBorrower {
+    id: ID!
+  }
+
+  input NewItemInput {
+    title: String!
+    description: String!
+    tags: [AssignedTag]!
+  }
+
+  type Query {
+    user(id: ID!): User
+    viewer: User
+    items(filter: ID): [Item]
+    tags: [Tag]
+  }
+
+  type Mutation {
+    addItem (
+      item: NewItemInput!
+    ): Item
+    signup: Boolean
+    login: Boolean
+    logout: Boolean
+  }
+`;
